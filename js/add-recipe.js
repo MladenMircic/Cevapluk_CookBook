a3648f1057d7cdce43d3a2af9a4bb78bc15d7fb3
MladenMircic <mm180413d@student.etf.bg.ac.rs>
2021-09-19 22:11:30 +0200

    Sredjeni komentari i dodavanje recepata

--- js/add-recipe.js
+++ js/add-recipe.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    let language = localStorage.getItem("language");
+
     $(".send-recipe-info").click(function() {
         let food_type = $("#food-type").val();
         let food_name = $("#food-name").val();
@@ -21,8 +23,35 @@ $(document).ready(function() {
                 localStorage.setItem("failed", 1);
             }
 
-        food_type = food_type.toLowerCase().replace(' ', '_');
-        let food_type_arr = JSON.parse(localStorage.getItem(food_type));
+        let food_type_eng;
+        let food_type_srb;
+        switch (food_type) {
+            case 'Predjelo':
+            case 'Appetizer': {
+                food_type_eng = JSON.parse(localStorage.getItem("appetizer"));
+                food_type_srb = JSON.parse(localStorage.getItem("predjelo"));
+                break;
+            }
+            case 'Glavno jelo':
+            case 'Main course': {
+                food_type_eng = JSON.parse(localStorage.getItem("main_course"));
+                food_type_srb = JSON.parse(localStorage.getItem("glavno_jelo"));
+                break;
+            }
+            case 'Dezert':
+            case 'Dessert': {
+                food_type_eng = JSON.parse(localStorage.getItem("dessert"));
+                food_type_srb = JSON.parse(localStorage.getItem("dezert"));
+                break;
+            }
+            case 'Uzina':
+            case 'Snack': {
+                food_type_eng = JSON.parse(localStorage.getItem("snack"));
+                food_type_srb = JSON.parse(localStorage.getItem("uzina"));
+                break;
+            }
+        }
+        
 
         let steps_arr = steps.split("\n");
         let ingredients_arr = ingredients.split("\n");
@@ -35,11 +64,38 @@ $(document).ready(function() {
             steps: steps_arr,
             ingredients: ingredients_arr,
             stars: stars,
-            comments: []
+            comments: [],
+            video: 'https://www.youtube.com/embed/MtN1YnoL46Q'
         };
 
-        food_type_arr.push(new_recipe);
-        localStorage.setItem(food_type, JSON.stringify(food_type_arr));
+        food_type_eng.push(new_recipe);
+        food_type_srb.push(new_recipe);        
+        switch (food_type) {
+            case 'Predjelo':
+            case 'Appetizer': {
+                localStorage.setItem("appetizer", JSON.stringify(food_type_eng));
+                localStorage.setItem("predjelo", JSON.stringify(food_type_srb));
+                break;
+            }
+            case 'Glavno jelo':
+            case 'Main course': {
+                localStorage.setItem("main_course", JSON.stringify(food_type_eng));
+                localStorage.setItem("glavno_jelo", JSON.stringify(food_type_srb));
+                break;
+            }
+            case 'Dezert':
+            case 'Dessert': {
+                localStorage.setItem("dessert", JSON.stringify(food_type_eng));
+                localStorage.setItem("dezert", JSON.stringify(food_type_srb));
+                break;
+            }
+            case 'Uzina':
+            case 'Snack': {
+                localStorage.setItem("snack", JSON.stringify(food_type_eng));
+                localStorage.setItem("uzina", JSON.stringify(food_type_srb));
+                break;
+            }
+        }
 
         let my_account = JSON.parse(localStorage.getItem("my-account"));
         my_account.recipes.push(new_recipe);
